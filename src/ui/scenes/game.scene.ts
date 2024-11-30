a6d88573e8910600a5c0e21029e61842d0190424
republichenko <republichenko@gmail.com>
2024-11-30 14:57:07 +0200

    remove loader and manager

--- src/ui/scenes/game.scene.ts
+++ src/ui/scenes/game.scene.ts
@@ -1,5 +1,5 @@
 import { PixiContainer, PixiSprite, PixiSoundLibrary, PixiText } from "../../plugins/engine";
-import { Manager, SceneInterface } from "../../entities/manager";
+import { Manager, SceneInterface } from "../../plugins/engine/manager";
 import { Assets } from "pixi.js";
 
 export class GameScene extends PixiContainer implements SceneInterface {
