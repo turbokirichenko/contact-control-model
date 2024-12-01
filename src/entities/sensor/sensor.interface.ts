bd5399f36a9e14ff35517f8b38582d68122a7cc7
republichenko <republichenko@gmail.com>
2024-12-01 23:26:44 +0200

    control changed

--- src/entities/sensor/sensor.interface.ts
+++ src/entities/sensor/sensor.interface.ts
@@ -1,3 +1,22 @@
 import { AgentInterface } from "../agent/agent.interface";
 
-export interface SensorInterface extends AgentInterface {}
+export interface DecodedSignal {
+    prevHash: string;
+    scanHash: string;
+    date: Date;
+}
+
+export interface InputDevice {
+    scan(): DecodedSignal;
+}
+
+export interface StreamDevice {
+    streamWave(): boolean;
+}
+
+export interface SensorInterface extends AgentInterface {
+    inputDevice: InputDevice;
+    streamDevice: StreamDevice;
+    storage: DecodedSignal[];
+    getInfograph(): object;
+}
