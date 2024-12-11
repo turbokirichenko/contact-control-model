f077f8187c86cb6fa6df12703ee91c5c538f8bb4
republichenko <republichenko@gmail.com>
2024-12-11 13:55:20 +0200

    changed config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './style.css';
 import '@pixi/gif';
-import { App } from './app';
+import { App, model } from './app';
 import { FILL_COLOR } from './shared/constant/constants';
 import { Manager } from './plugins/engine/manager';
 import { IPixiApplicationOptions, PixiAssets } from './plugins/engine';
@@ -8,8 +8,6 @@ import { Loader } from './plugins/engine/loader';
 import { options } from './shared/config/manifest';
 import { LoaderScene } from './ui/scenes/loader.scene';
 import { ExampleScene } from './ui/scenes/example.scene';
-import { ModelSpawner } from './plugins/htmodel/main';
-import { modelExpantionConfig } from './shared/config/model-expantion.config';
 
 const boostsrap = async () => {
     const canvas = document.getElementById("pixi-screen") as HTMLCanvasElement;
@@ -33,7 +31,6 @@ const boostsrap = async () => {
     const loaderScene = new LoaderScene();
     Manager.changeScene(loaderScene);
     loader.download(options, loaderScene.progressCallback.bind(loaderScene)).then(() => {
-        const model = ModelSpawner.define(modelExpantionConfig);
         Manager.changeScene(new ExampleScene(model));
     });
 }
